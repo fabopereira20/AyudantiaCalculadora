5c098d466b0a58c612563deb51f01807806e15f6
fabopereira20 <f.pereira02@ufromail.cl>
2021-06-06 20:33:30 -0400

    tweaks

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    /*private static Main vista;
+    /*static Main vista;
+
     @BeforeAll
     static void beforeAll() {
         vista = new Main();
